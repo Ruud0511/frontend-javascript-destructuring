2b877454dc5ed4c63988e37477f4c1dd4acc3db4
Ruud0511 <post@ruud.co>
2023-08-11 15:47:23 +0200

    Opdrachten gemaakt, 6 en 7 waren iets lastiger

--- destructuring.js
+++ destructuring.js
@@ -6,6 +6,11 @@ const product = {
   screenType: 'LED',
 }
 
+// Destructuring opdracht 1
+const {price, refreshRate, screenType} = product;
+console.log(price, refreshRate);
+
+
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
 
 const productInformation = {
@@ -17,6 +22,10 @@ const productInformation = {
   screenQuality: 'Ultra HD/4K',
   smartTv: true,
 }
+
+// Destructuring opdracht 2
+const {type, brand} = productInformation.general;
+console.log(type, brand);
 
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
 
@@ -38,17 +47,32 @@ const tvOptions = {
   },
 };
 
+// Destructuring opdracht 3
+const { wifi, bluetooth } = tvOptions.options.connectivity.popular;
+console.log(wifi, bluetooth);
+
+
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
 
 const connectivity = ['wifi', 'bluetooth'];
+
+// Destructuring opdracht 4
+const [first, second] = connectivity;
+console.log(first, second);
+
 
 
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
 
 const tvSizes = [41, 43, 46, 56, 65];
 
+// Destructuring opdracht 5
 
-// opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
+const [size1, size2, size3, size4, size5] = tvSizes;
+console.log(size1, size3);
+
+
+// opdracht 6: Destructure name en address uit de return value van deze functie en log ze daarna in de terminal
 
 function getCompanyDetails() {
   return {
@@ -57,8 +81,21 @@ function getCompanyDetails() {
   }
 }
 
-// opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
+// Destructuring opdracht 6: kijk hierbij heel goed of een object niet al eerder is gelogd in een andere destructured variable, anders crasht je code!
+const { name, adress } = getCompanyDetails();
+console.log(name, adress);
+
+// Je moet hier wel goed opletten dat de name niet eerder  in opdracht 2 in de destructured variabele is gezet, anders crasht deze. Dit is denk ik wel goed om duidelijk te maken en dat een van de 'name' beter een andere naam had kunnen hebben!
+
+
+// opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal.
+// Let op: de functie verwacht argumenten bij het aanroepen!
 
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+
+// Destructuring opdracht 7: denk meegeven  parameters bij getDetails ('Ruud' en 42)!
+const [nameDetails, ageDetails] = getDetails('Ruud', 42);
+console.log(nameDetails);
+console.log(ageDetails);
